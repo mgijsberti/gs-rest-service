d8e22e8e11950e0cbd2844d037c983e24f056fe7
mgijsberti <mgijsberti@gmail.com>
2014-07-31 13:05:42 +0200

    Added simple cors rest service

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Greeting {
+
+    private final long id;
+    private final String message;
+
+    public Greeting(long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
